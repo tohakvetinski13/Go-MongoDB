27bd14fde0c1525164ab685f4d054e588bd87650
tohakvetinski13 <76660748+tohakvetinski13@users.noreply.github.com>
2021-05-11 19:59:39 +0300

    changed defer cancel()

--- Connected/main.go
+++ Connected/main.go
@@ -18,12 +18,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer client.Disconnect(ctx)
+	defer cancel()
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
