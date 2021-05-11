628ac89edf0f3b400564c3f8948135279f786f31
tohakvetinski13 <76660748+tohakvetinski13@users.noreply.github.com>
2021-05-11 19:52:10 +0300

    initial commit

---
+++ Connected/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	// ATLAS_URI your personal URI in Atlas Cluster
+	client, err := mongo.NewClient(options.Client().ApplyURI(ATLAS_URI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, v := range databases {
+		fmt.Println(i, v)
+	}
+
+}
